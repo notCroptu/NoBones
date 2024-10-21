8ae4eaf50240b1963be8fab67d4963529a567e68
André Leandro <imandreleandro@gmail.com>
2024-10-21 14:10:08 +0100

    mariana catch this

---
+++ Assets/Scripts/CatText.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using static Unity.Burst.Intrinsics.X86.Avx;
+
+public class CatText : MonoBehaviour
+{
+    private TMP_Text tmp;
+
+    void Start()
+    {
+        tmp = transform.Find("Text").GetComponent<TMP_Text>();
+        StartCoroutine(ChangeTextCat());
+    }
+
+    private IEnumerator ChangeTextCat()
+    {
+        yield return new WaitForSeconds(1.0f);
+        while (true)
+        {
+            StartCoroutine(ChangeText("Hello!", 1.0f));
+        }
+    }
+
+    private IEnumerator ChangeText(string text, float time)
+    {
+        int count = 0;
+        while (true)
+        {
+            count++;
+            tmp.text += text.Substring(count-1, count);
+            yield return new WaitForSeconds(time);
+        }
+    }
+
+}
